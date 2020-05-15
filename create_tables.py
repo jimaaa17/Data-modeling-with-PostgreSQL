8e5ff0a807754e177b1e5b71a5cfefa0dabd4cc2
Jimit Naik <jimaaa17@users.noreply.github.com>
2020-05-15 01:32:33 -0400

    "Jimit"

--- create_tables.py
+++ create_tables.py
@@ -9,13 +9,14 @@ def create_database():
     """
     
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     conn.set_session(autocommit=True)
-    cur = conn.cursor()
+    cur = conn.cursor() 
+
     
     # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    #cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    #cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
 
     # close connection to default database
     conn.close()    
--- sql_queries.py
+++ sql_queries.py
@@ -79,12 +79,12 @@ VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration)
-VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;
+VALUES (%s, %s, %s, %s, %s) ON CONFLICT(song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, lattitude, longitude)
-VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;
+VALUES (%s, %s, %s, %s, %s) ON CONFLICT(artist_id) DO NOTHING;
 """)
 
 
